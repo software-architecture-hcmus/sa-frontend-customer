61d4f063163b536b4e920d82dbe975e2b5d4f2cc
tutam <damtutam@gmail.com>
2025-01-12 15:37:28 +0700

    fix: unauthorized at first load home page

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -9,6 +9,14 @@ const apiClient = axios.create({
     },
 });
 
+apiClient.interceptors.request.use((config) => {
+    const token = localStorage.getItem(ACCESS_TOKEN);
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
+
 //TODO: Add interceptor to refresh token
 
 export default apiClient;
